b2476a4c379dbc4a849477328745a7ecfa5a1b8b
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-02-09 09:04:06 +0300

    made required changes

--- client.py
+++ client.py
@@ -42,7 +42,10 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return price_a/price_b
+    if price_b == 0:
+        return
+    else:
+        return price_a / price_b
 
 
 # Main
@@ -57,4 +60,4 @@ if __name__ == "__main__":
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-    print("Ratio %s" % (getRatio(prices['ABC'], prices['DEF'])))
+    print("Ratio %s" % (getRatio(prices["ABC"], prices["DEF"])))
